9b346311d2594bc7836fe8594ac258916ddc707f
Abundance Esim <abundanceesim@gmail.com>
2023-10-12 00:47:35 -0400

    Shuffle answers list

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -4,4 +4,10 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers(){
+    final shuffledList = List.of(answers); //original list
+    shuffledList.shuffle(); //new list is created here
+    return shuffledList; //return new list.
+  }
 }
